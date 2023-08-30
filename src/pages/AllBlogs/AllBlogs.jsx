94382b6e39829f5d9c8f34e5cfdfcca4e5251494
Anup Tachamo <anuptachamo@gmail.com>
2023-08-30 20:51:18 +0545

    css modify of Allblogs

--- src/pages/AllBlogs/AllBlogs.jsx
+++ src/pages/AllBlogs/AllBlogs.jsx
@@ -24,12 +24,9 @@ const AllBlogs = () => {
   return (
     <>
     <Navbar/>
-    {/* <div>
+    <div>
       <h1>My Blogs</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente vitae eum suscipit, voluptates officia aut voluptas laboriosam accusantium, eos sint atque quo placeat quaerat nam est nesciunt minus soluta in.
-      Obcaecati iste voluptatibus vel commodi corporis repellat culpa maiores impedit eum sit. Illo neque veniam tempore reprehenderit sint inventore, eius quo molestias quod eaque dolorem officiis soluta libero aspernatur? Quam.
-      Reiciendis, vero deserunt sed explicabo id quibusdam? Quisquam, eveniet sint neque repellat aperiam delectus velit omnis, blanditiis totam porro ducimus doloremque assumenda, maxime et provident rem molestias vitae recusandae corrupti.</p>
-    </div> */}
+    </div>
 
     <div style={{display:'flex',justifyContent:"space-evenly",flexWrap:'wrap'}}>
         {
@@ -37,10 +34,10 @@ const AllBlogs = () => {
             return (
               <div className="card" key={blog.id}>
                 <img src={blog.avatar} alt="Avatar"  width='100%' />
-                  <div className="container">
-                    <h4><b>{blog.title}</b></h4> 
-                    <p style={{color:"red"}}>{blog.Description}</p> 
-                    <p>{blog.createdAt}</p>
+                  <div className="card-content">
+                    <h4 className="card-title">{blog.title}</h4> 
+                    <p className='card-description'>{blog.Description}</p> 
+                    <p className="card-date">{blog.createdAt}</p>
                   </div>
               </div>
             )
