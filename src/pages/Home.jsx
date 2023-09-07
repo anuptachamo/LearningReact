6f36f5351ff1261ee1b6598ed5d75d1c92539e37
Anup Tachamo <anuptachamo@gmail.com>
2023-09-07 18:10:37 +0545

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -36,6 +36,8 @@ const Home = () => {
       Delectus at maxime voluptatibus quis minus qui aut ipsam, totam deleniti mollitia porro similique doloribus doloremque vel omnis voluptatem enim iusto. Architecto impedit, modi eligendi ipsum excepturi quae maxime fugiat?
       Odio nobis cum natus praesentium assumenda nihil, itaque fuga tempore commodi! Harum commodi laboriosam iste error, distinctio aut quasi quas tenetur. Autem, repudiandae sit fugiat odio soluta pariatur perspiciatis iste.
       Delectus consectetur, nihil distinctio voluptatibus, saepe possimus recusandae, inventore quas sed ducimus error doloremque laboriosam molestiae in. Consequatur, nemo aut mollitia unde suscipit dolore dicta asperiores? Placeat ipsa ad quam.
+
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque iusto recusandae omnis ab odit hic corporis, at a soluta nostrum, quam deleniti vel eos repellendus optio rem mollitia quas esse.
       </p>
       <hr />
      
